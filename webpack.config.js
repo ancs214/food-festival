ddb14ddff6593070904debcd74b14cef581376f4
ancs214 <ancs214@gmail.com>
2022-06-08 16:33:25 -0400

    added image webpack loader npm

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ const BundleAnalyzerPlugin = require("webpack-bundle-analyzer").BundleAnalyzerPl
 const path = require("path");
 const webpack = require("webpack");
 
-module.exports = {
+const config = {
   entry: {
     app: "./assets/js/script.js",
     events: "./assets/js/events.js",
@@ -10,9 +10,36 @@ module.exports = {
     tickets: "./assets/js/tickets.js"
   },  
     output: {
-      path: path.resolve(__dirname, 'dist'),
+      path: path.resolve(__dirname + '/dist'),
       //The name of each attribute in the entry object will be used in place of [name] in each bundle.js file that is created.
       filename: "[name].bundle.js"
+    },
+    module: {
+      //This object will identify the type of files to pre-process using the test property to find a regex (any img file w extension of .jpg)
+      rules: [
+        {
+          test: /\.jpg$/i,
+          use: [
+            {
+              loader: "file-loader",
+              //to rename our dist/img files and re-path them to dist/assets folder
+              options: {
+                esModule: false,
+                name (file) {
+                  return "[path][name].[ext]"
+                },
+                publicPath: function(url) {
+                  return url.replace("../", "/assets/")
+                }
+              }  
+            },
+            {
+              loader: 'image-webpack-loader'
+            }
+          ]
+          
+        }
+      ]
     },
     plugins: [
         new webpack.ProvidePlugin({
@@ -25,3 +52,5 @@ module.exports = {
     ],
     mode: 'development'
   };
+
+  module.exports = config;
